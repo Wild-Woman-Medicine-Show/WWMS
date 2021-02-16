42c99f9c867a831fc01df4745fbcd991c9b6ef6a
Tiny Giant <ajsa.ferguson@gmail.com>
2021-02-16 15:31:12 -0800

    Add more files

---
+++ Product Manager/app/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    icon: path.join(__dirname, "icon.png")
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
