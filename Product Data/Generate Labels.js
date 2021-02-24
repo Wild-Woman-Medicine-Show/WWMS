ab8e23a270b4f3f2680f9a5356e0dcef91ee0cc3
Tiny Giant <ajsa.ferguson@gmail.com>
2021-02-24 10:20:02 -0800

    Update

--- Product Data/Generate Labels.js
+++ Product Data/Generate Labels.js
@@ -1,66 +1,54 @@
-function remove_cutline(doc) {
-	var items = doc.pageItems
-	if(!items) return
-
-	var cutline = items.getByName("Cutline")
-	cutline.remove()
-}
-
-function get_template_data(doc) {
-	var split = doc.name.substring(0, doc.name.lastIndexOf('.')).split(' - ')
-	return {
-		type: split[1],
-		size: split[2],
-		line: split[3],
-		blend: 5 in split ? split[4] : null,
-		position: 5 in split ? split[5] : split[4]
-	}
-}
-
-function get_output_name(doc, data) {
-	var blend = data.blend
-	if(doc.variables.getByName('Title')) blend = doc.variables.getByName('Title').pageItems[0].contents.replace('/',' ')
-	return "WWMS" + 
-           " - " + data.type + 
-           " - " + data.size + 
-           " - " + data.line + 
-           (data.blend ? " - " + data.blend : "") +
-           (data.position ? " - " + data.position : "")
-}
-function get_output_folder(folder) {
-	return new Folder(folder.absoluteURI.replace('Template', 'Label'))
-}
+var globals = {}
 
 function is_folder(folder) {
 	var is_folder = false
+
 	try {
 		folder.getFiles('*')
 		is_folder = true
 	} catch(e) {}
+
 	return is_folder
 }
 
-function export_template(template, dataset, data) {
-	dataset.display()
-	var output = get_output_folder(new Folder(template.path))
-	if(!output.exists) output.create()
+function generate_file_name() {
+	var blend = globals.template.variables.getByName('Title').pageItems[0].contents.replace('/',' ')
+	var line = globals.template.variables.getByName('Line').pageItems[0].contents
+	return globals.template_name.replace(line, line + ' - ' + blend)
+}
+
+function export_template() {
+	var output_eps_target = new File(generate_file_name().replace('Templates', 'Labels').replace(".ai", '.eps'))
+	var output_eps_folder = new Folder(output_eps_target.path)
+	if(!output_eps_folder.exists) output_eps_folder.create()
+
+	var output_png_target = new File(generate_file_name().replace('Templates', 'Images').replace(".ai", '.png'))
+	var output_png_folder = new Folder(output_png_target.path) 
+	if(!output_png_folder.exists) output_png_folder.create()
+
+	globals.cutline.hidden = true
+
+	globals.template.exportFile(output_png_target, ExportType.PNG24)
+
+	globals.cutline.hidden = false
+
 	var saveopts = new EPSSaveOptions()
 	saveopts.cmykPostScript = true
     saveopts.embedAllFonts = true
     saveopts.embedLinkedFiles = true
-	template.saveAs(
-		new File(output.absoluteURI + "/" + get_output_name(template, data) + ".eps"), 
-		saveopts
-	)
+
+	globals.template.saveAs(output_eps_target, saveopts)
 }
 
-function iterate_datasets(template, datasets, data, cb) {
+function iterate_datasets(cb) {
+	var datasets = globals.template.dataSets
 	if(datasets.length) {
 		for(var index = datasets.length - 1; index > -1; --index) {
-			cb(template, datasets[index], data)
+			datasets[index].display()
+			cb()
 		}
 	} else {
-		cb(template, { display: function() {} }, data)
+		cb()
 	}
 }
 
@@ -72,16 +60,20 @@ function iterate_directory(folder) {
 			iterate_directory(directory[index])
 		}
 	} else if(/\.ai$/.test(folder.name) > -1) {
-	    var template = app.open(folder)
+	    globals.template = app.open(folder)
+	    globals.template_name = globals.template.fullName.toString()
 
-		iterate_datasets(template, template.dataSets, get_template_data(template), export_template)
+	    var items = globals.template.pageItems
+		globals.cutline = {}
+		if(items) globals.cutline = items.getByName("Cutline")
 
-		template.close(SaveOptions.DONOTSAVECHANGES)
+		iterate_datasets(export_template)
+
+		globals.template.close(SaveOptions.DONOTSAVECHANGES)
 	}
 }
 
 (function main() {
-	var input = new Folder('~/WWMS/Product Templates').selectDlg("Select input folder")
-	if(!input) return {}
-	iterate_directory(input)
+	globals.folder = new Folder('~/WWMS/Product Templates').selectDlg("Select input folder")
+	if(globals.folder) iterate_directory(globals.folder)
 })()
